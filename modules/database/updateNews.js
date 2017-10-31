29ec55d3abf3e9e97226a2cac4fc9937185e2d8b
yanbnuep <yanbnuep@gmail.com>
2017-10-31 08:11:13 +0800

    uploadNews

--- modules/database/updateNews.js
+++
@@ -1,3 +0,0 @@
-const db = require('./dbSetting')
-const squel = require('squel')
-const bcrypt =  requie('bcrypt');
--- routes/updateNews.js
+++
@@ -1,9 +0,0 @@
-const router = require('koa-router')()
-
-router.prefix('/updateNews')
-
-router.get('/',function(ctx,next){
-	await ctx.render('/news/udateNews',{
-		title: 'first router'
-	})
-})
