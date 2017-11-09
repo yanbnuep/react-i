8c998162d05b7bcb30f24823748ba868a27a2614
yanbnuep <yanbnuep@gmail.com>
2017-11-09 18:05:08 +0800

    news react

---
+++ modules/news/react/components/listItem.jsx
@@ -0,0 +1,37 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+
+module.exports = React.createClass({
+	render() {
+		return (
+				<li className="newsItemLi">
+
+				</li>
+			)
+	}
+})
+
+
+
+class Cover extends Component {
+	render () {
+		return (
+				<div className="coverContain">
+					<img src={this.props.coverSource} className="coverImg"/>
+				</div>
+			)
+	}
+}
+
+
+class Detail extends Component {
+	render() {
+		return (
+				<div className="newsDetail">
+					<a className"newsTitle" onClick={this.props.handleClick}>{this.props.title}</a>
+					<div className"newsDescription">{this.props.description}</div>
+					<div className"newsDate">{this.props.date}</div>
+				</div>
+			)
+	}
+}
